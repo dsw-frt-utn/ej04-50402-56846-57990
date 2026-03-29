d57cfe7f6bb44f4cd0a25c5805374fa00149ea59
Francisco Vicente <franciscovicente@doc.frt.utn.edu.ar>
2026-03-29 12:13:25 -0300

    Nueva clase para representar las marcas de vehículos

---
+++ src/main/java/domain/Marca.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Marca {
+    private String nombre;
+
+    public Marca(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
