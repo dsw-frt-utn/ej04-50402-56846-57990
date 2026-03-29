878ea85323d942be712ca9bf8d09fcba217b1938
Francisco Vicente <franciscovicente@doc.frt.utn.edu.ar>
2026-03-29 12:08:17 -0300

    Código base de la solución

---
+++ src/main/java/domain/VehiculoCombustible.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class VehiculoCombustible extends Vehiculo {
+    private double kilometrosPorLitro;
+    private double litrosExtra;
+
+    public VehiculoCombustible(String patente, String marca, String modelo, int anio, double capacidadCarga,
+                               Sucursal sucursal, double kilometrosPorLitro, double litrosExtra) {
+        super(VehiculoTipo.COMBUSTIBLE, patente, marca, modelo, anio, capacidadCarga, sucursal);
+        this.kilometrosPorLitro = kilometrosPorLitro;
+        this.litrosExtra = litrosExtra;
+    }
+    
+     public double getKilometrosPorLitro() {
+        return kilometrosPorLitro;
+    }
+
+    public double getLitrosExtra() {
+        return litrosExtra;
+    }
+
+    @Override
+    public double calcularConsumo(double kilometros) {
+        double total = kilometrosPorLitro - litrosExtra;
+        return total;
+    }
+}
---
+++ src/main/java/domain/VehiculoElectrico.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class VehiculoElectrico extends Vehiculo {
+    private double kwhBase;
+
+    public VehiculoElectrico(String patente, String marca, String modelo, int anio, double capacidadCarga,
+                             Sucursal sucursal, double kwhBase) {
+        super(VehiculoTipo.ELECTRICO, patente, marca, modelo, anio, capacidadCarga, sucursal);
+        this.kwhBase = kwhBase;
+    }
+
+    public double calcularConsumo() {
+        double total = kwhBase;
+
+        if (capacidadCarga >= 1200) {
+            total = total + 0.15;
+        }
+
+        return total;
+    }
+}
